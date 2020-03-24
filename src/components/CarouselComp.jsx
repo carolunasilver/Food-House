a33d3cdfcd53408ce0c2c1d2aca6720e94919d00
carolunadsilver <carolinadsilver@gmail.com>
2020-03-24 15:22:17 -0400

    Carousel updated

--- src/components/CarouselComp.jsx
+++ src/components/CarouselComp.jsx
@@ -15,7 +15,7 @@ class CarouselComp extends Component {
                     />
                     <Carousel.Caption>
                     <h1 style={{textAlign: "center", color: "orangered", fontWeight: "bold", fontSize: "50px"}}>Don't know what to cook but feeling adventurous?</h1>
-                    <p style={{textAlign: "center", color: "black", fontSize: "22px", fontWeight: "bold"}}>Click on Random Recipes and surprise your guests!</p>
+                    <p style={{textAlign: "center", color: "black", fontSize: "22px", fontWeight: "bold"}}>Click on Recipe of the Day and surprise your guests!</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -26,7 +26,7 @@ class CarouselComp extends Component {
                     />
                     <Carousel.Caption>
                     <h1 style={{textAlign: "center", color: "black", fontWeight: "bold", fontSize: "50px"}}>In need of Inspiration?</h1>
-                    <p style={{textAlign: "center", fontWeight: "bold"}}>Use the search bar to type a main ingredient and find easy recipes...</p>
+                    <p style={{textAlign: "center", fontSize: "22px", fontWeight: "bold"}}>Use the search bar to type a main ingredient and find easy recipes...</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -37,7 +37,7 @@ class CarouselComp extends Component {
                     />
                     <Carousel.Caption>
                     <h1 style={{textAlign: "center", color: "orangered", fontWeight: "bold", fontSize: "50px"}}>Do you find complicated to follow recipes? </h1>
-                    <p style={{textAlign: "center", fontWeight: "bold"}}>Watch the video under the Recipe.</p>
+                    <p style={{textAlign: "center", fontSize: "22px", fontWeight: "bold"}}>Watch the video under the Recipe.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
